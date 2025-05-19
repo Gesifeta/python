49cbaa48f200b80912c938d360970f2be5823471
gesifeta <adamgemechu@gmail.com>
2025-05-19 10:02:39 +0300

    refactored caeser cipher

--- day_8/caeser_cipher.py
+++ day_8/caeser_cipher.py
@@ -1,12 +1,9 @@
-def caeser_cipher():
+def caeser_cipher(method,message,shift):
    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-  
    encrypted = ''
    decrypted = ''
-   method = input("Type 'encode' to encrypt, and 'decode' to decrypt ")
+#    method = input("Type 'encode' to encrypt, and 'decode' to decrypt ")
    if method.lower() == 'encode':
-      message = input("Enter the message to encode ")
-      shift =  int(input("Type number of shifts "))
     #   loop thru the message and replace it with the next shifts from the letter
       for char in message:
          if char == ' ':
@@ -15,8 +12,8 @@ def caeser_cipher():
             encrypted += letters[letters.index(char) - (shift)]
       print("here is your encrypted = ", encrypted)
    elif method.lower() == 'decode':
-      message = input("Enter the message to decode ")
-      shift =  int(input("Type number of shifts "))
+    #   message = input("Enter the message to decode ")
+    #   shift =  int(input("Type number of shifts "))
       for char in message:
           if char == ' ':
               decrypted += ' '
@@ -28,7 +25,15 @@ def caeser_cipher():
       print("Here is your decode message: ", decrypted)
    else:
       print("invalid input")
-caeser_cipher()
+continue_ciphering = 'Y'
+
+while continue_ciphering == "Y" or continue_ciphering == 'y':
+    method = input("Type 'encode' to encrypt, and 'decode' to decrypt ")
+    message = input("Enter the message to decode ")
+    shift =  int(input("Type number of shifts "))
+    caeser_cipher(method,message,shift)
+    continue_ciphering = input("Would you like to continue ? Y/N :")
+   
       
 
       
