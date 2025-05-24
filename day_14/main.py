0e39f5ca28a637c03b98bfc698e6103b66a2cee6
gesifeta <adamgemechu@gmail.com>
2025-05-24 12:04:32 +0300

    coffee ording machine

--- day_14/main.py
+++ day_14/main.py
@@ -10,7 +10,10 @@ def get_players():
         player_2 = random.randint(0, len(data)-1)
     return [data[player_1],data[player_2]]
 # compare the two players
-def check_winner():
+
+def check_winner(score):
+    get_players()
+   
     player = get_players()
     print(f"Compare A: {player[0]['name']}, {player[0]['description']}, from {player[0]['country']}")
     print(vs)
@@ -19,30 +22,26 @@ def check_winner():
     play = input("Who has more followers? Type 'A' or 'B': ").lower()
     if play == 'a':
         if player[0]['follower_count'] > player[1]['follower_count']:
-            return True
+            score += 1
         else:
-             return False
+            return [score, False]
     elif play == 'b':
          if player[1]['follower_count'] > player[0]['follower_count']:
-            return True
+            score += 1
          else:
-             return False
-    return False
+            return [score, False]
+    return [score,False]
 # calculat scores
-def calculate_score():
-    if check_winner() == True:
-        return 1
-    else:
-        return 0
+
 def main():
     print(logo)
     score = 0
-    while check_winner() != False:
-        get_players()
-        check_winner()
-        score += calculate_score()
-        print("Score ===> ",score)
-       
+    print(check_winner(score))
+    while check_winner(score)[1] !=False:
+        score = check_winner(score)[1]
+        check_winner(score)
     print(f"That is wrong. Final score is : {score}")
+
+  
 
 main()
