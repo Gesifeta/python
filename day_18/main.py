ab4d191c70a695cd8fc76d0f6c13ceaa664da8fb
gesifeta <adamgemechu@gmail.com>
2025-05-28 19:46:16 +0300

    shape generator

--- day_18/main.py
+++ day_18/main.py
@@ -1,16 +1,16 @@
-from turtle import Turtle, Screen, Screen
-import  random
-
-
-
-timmy = Turtle()
+from turtle import  Turtle,Screen
+import random
+timmy_the_turtle = Turtle()
 screen = Screen()
-random = random.random()
-for i in range(100):
-    steps = int(random * 100)
-    angle = int(random* 360)
-    timmy.right(angle)
-    timmy.fd(steps)
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("green")
 
-timmy.screen.mainloop()
-screen.exitonclick()
+def draw_shapes(number_of_sides):
+    angles = round(360/number_of_sides,0)
+    for _ in range(0,number_of_sides):
+        timmy_the_turtle.forward(50)
+        timmy_the_turtle.right(angles)
+for num in range(3,10):
+    draw_shapes(num)
+
+
