6b617f18f34d541a7752da5adc18762526ee0d47
gesifeta <adamgemechu@gmail.com>
2025-05-29 06:07:41 +0300

    random walk

--- day_18/main.py
+++ day_18/main.py
@@ -1,3 +1,4 @@
+from random import choice
 from turtle import  Turtle,Screen
 import random
 timmy_the_turtle = Turtle()
@@ -5,12 +6,18 @@ screen = Screen()
 timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color("green")
 
-def draw_shapes(number_of_sides):
-    angles = round(360/number_of_sides,0)
-    for _ in range(0,number_of_sides):
-        timmy_the_turtle.forward(50)
-        timmy_the_turtle.right(angles)
-for num in range(3,10):
-    draw_shapes(num)
+colors =["blue","bisque","orange","pink","purple","mediumOrchid"]
+directions =[0, 90, 180, 270]
+timmy_the_turtle.pensize(20)
+timmy_the_turtle.speed("slowest")
+for _ in range(1,100):
+    timmy_the_turtle.color(random.choice(colors))
+    timmy_the_turtle.forward(10)
+    timmy_the_turtle.setheading(random.choice(directions))
+    timmy_the_turtle.forward(20)
+
+
+
+screen.exitonclick()
 
 
