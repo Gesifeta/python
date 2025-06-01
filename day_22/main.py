c61ddbf594744dec415999e4e9cb0058ad04049e
gesifeta <adamgemechu@gmail.com>
2025-06-01 11:34:48 +0300

    refactored to class

--- day_22/main.py
+++ day_22/main.py
@@ -1,37 +1,24 @@
 from turtle import Screen,Turtle
+
+from paddle import Paddle
 import  random
 screen = Screen()
-paddle = Turtle()
 screen.bgcolor("black")
 screen.title("Pong")
 screen.listen()
 screen.tracer(0)
-
 screen.setup(height=600,width=800)
+r_paddle = Paddle((350,0))
+l_paddle = Paddle((-350,0))
 
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5,stretch_len=1)
-paddle.pu()
-paddle.goto(350,0)
-screen.update()
-def move_up():
-    new_y = paddle.ycor() + 20
-    if new_y >= 300:
-        return
-    paddle.goto(paddle.xcor(),new_y)
-def move_down():
-    new_y = paddle.ycor() - 20
-    if new_y <= -300:
-        return
-    paddle.goto(paddle.xcor(),new_y)
 
-def move_paddle():
-    is_game_on = True
-    while is_game_on:
-        paddle.goto(350,random.randint(-300,350),)
-        paddle.speed("slowest")
-# move_paddle()
-screen.onkey(move_up,"Up")
-screen.onkey(move_down,"Down")
+
+
+screen.onkey(l_paddle.move_up,"w")
+screen.onkey(l_paddle.move_down,"s")
+screen.onkey(r_paddle.move_up,"Up")
+screen.onkey(r_paddle.move_down,"Down")
+game_is_on =True
+while game_is_on:
+    screen.update()
 screen.exitonclick()
--- day_22/paddle.py
+++ day_22/paddle.py
@@ -1,6 +1,6 @@
 from turtle import  Turtle
 
-class paddle(Turtle):
+class Paddle(Turtle):
     def __init__(self,position):
         super().__init__()
         self.shape("square")
@@ -8,4 +8,15 @@ class paddle(Turtle):
         self.shapesize(stretch_wid=5, stretch_len=1)
         self.pu()
         self.goto(position)
-      
+
+    def move_up(self):
+        new_y = self.ycor() + 20
+        if new_y >= 300:
+            return
+        self.goto(self.xcor(), new_y)
+
+    def move_down(self):
+        new_y = self.ycor() - 20
+        if new_y <= -300:
+            return
+        self.goto(self.xcor(), new_y)
