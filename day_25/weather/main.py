fcf739e132572234b9b9f16a7d8589dbcebb1613
gesifeta <adamgemechu@gmail.com>
2025-06-09 16:59:57 +0300

    temperature converter

--- day_25/weather/main.py
+++ day_25/weather/main.py
@@ -3,12 +3,22 @@
 #     print(weather)
 
 import csv
+
+import pandas
+import math
 import pandas as pd
+from numpy.ma.extras import average
 
 with open("weather_data.csv") as csvfile:
     reader = csv.reader(csvfile)
     temperatures = []
     for row in reader:
         if row[1] != "temp":
-          temperatures.append((row[1]))
+          temperatures.append(int(row[1]))
     print(temperatures)
+data = pandas.read_csv("weather_data.csv")
+temp = data[data["day"]=="Monday"]
+monday_tem = temp["temp"][0]
+print(monday_tem)
+tem_ferheniet = int(monday_tem)*9/5 + 32
+print(tem_ferheniet)
