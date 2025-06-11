37c8892929fd7e4d2637a26fb16ff039a87b118b
gesifeta <adamgemechu@gmail.com>
2025-06-11 11:14:37 +0300

    added score board

--- day_25/us_states/main.py
+++ day_25/us_states/main.py
@@ -23,17 +23,27 @@ def guess_state():
 guessed_state = guess_state()
 all_state = state_lists["state"].to_list()
 game_is_over = False
+score = 0
+score_board = Turtle()
+score_board.hideturtle()
+score_board.penup()
+
 while not game_is_over:
-    guessed_state = guess_state()
     if guessed_state in all_state:
+        score_board.clear()
+        score += 1
+        score_board.goto(0, 270)
+        score_board.write(f"Score: {score}/50", align="center", font=("Arial", 20, "bold"))
         new_state = Turtle()
+        new_state.penup()
         new_state.shape("turtle")
         new_state.hideturtle()
-        new_state.penup()
         new_state.color(random.choice(colors))
         new_state.goto(state_lists[state_lists.state == guessed_state]["x"].to_list()[0],state_lists[state_lists.state == guessed_state]["y"].to_list()[0])
         new_state.write(guessed_state,False,align="center",font=("Arial",10,"bold"))
+        guessed_state = guess_state()
     if guessed_state == "exit":
         game_is_over = True
+
 
 screen.mainloop()
