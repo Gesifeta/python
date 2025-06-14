8f727b8d993c2db6a18e251665ab221bae85c674
gesifeta <adamgemechu@gmail.com>
2025-06-14 12:50:36 +0300

    finished mile converter

--- day_27/Other_Tkinter_Widgets.py
+++ day_27/Other_Tkinter_Widgets.py
@@ -47,6 +47,8 @@ spinbox.pack()
 #Called with current scale value.
 def scale_used(value):
     print(value)
+
+
 scale = Scale(from_=0, to=100, command=scale_used)
 scale.pack()
 
@@ -54,6 +56,8 @@ scale.pack()
 def checkbutton_used():
     #Prints 1 if On button checked, otherwise 0.
     print(checked_state.get())
+
+
 #variable to hold on to checked state, 0 is off, 1 is on.
 checked_state = IntVar()
 checkbutton = Checkbutton(text="Is On?", variable=checked_state, command=checkbutton_used)
@@ -63,6 +67,8 @@ checkbutton.pack()
 #Radiobutton
 def radio_used():
     print(radio_state.get())
+
+
 #Variable to hold on to which radio button value is checked.
 radio_state = IntVar()
 radiobutton1 = Radiobutton(text="Option1", value=1, variable=radio_state, command=radio_used)
