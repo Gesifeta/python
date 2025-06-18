3c956b4a3c89d14a0f7a1a70cedf8e7722daa337
gesifeta <adamgemechu@gmail.com>
2025-06-18 09:02:47 +0300

    pomodoro

---
+++ day_28/pomodoro/main.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+from tkinter import Label
+
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = tk.Tk()
+window.title("Pomodoro")
+window.config(padx=20, pady=20,width=400, height=400)
+title = Label(text="Timer", font=FONT_NAME , fg=GREEN, bg=YELLOW)
+
+window.mainloop()
