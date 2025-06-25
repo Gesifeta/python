ee27f7b417f27e67706079e1770b46bfa6c688a7
gesifeta <adamgemechu@gmail.com>
2025-06-25 11:14:34 +0300

    fiinished score logic

--- day_31/main.py
+++ day_31/main.py
@@ -4,9 +4,12 @@ import json
 import pandas as pd
 from tkinter import *
 import random
+import datetime
 
 BACKGROUND_COLOR = "#B1DDC6"
 FLASHES = 20
+wrong_answer = 0
+right_answer = 0
 # set file directory
 file_dir = os.path.dirname(__file__)
 # window for the display
@@ -30,8 +33,34 @@ canvas.grid(column=0,row=0,columnspan=2)
 background_image = canvas.create_image(405,270, image = card_front_image )
 title = canvas.create_text(400,150, text="swedish", font=("Arial",30,"italic"))
 meaning = canvas.create_text(400,263,text="english", font=("Arial",50,"bold"))
+current = datetime.datetime.now()
+
+scores ={
+    current.day:{
+        "wrong":0,
+        "right":0
+    }
+}
 
 
+def wrong_score():
+    scores[current.day]["wrong"] += 1
+    print(scores)
+
+def right_score():
+    scores[current.day]["right"] += 1
+    print(scores)
+
+
+def save_score():
+    try:
+        with open(f"{file_dir}/data/score.json","r") as new_score:
+            score = json.load(new_score)
+            score.update(scores)
+            json.dump(new_score,score)
+    except FileNotFoundError:
+        with open(f"{file_dir}/data/score.json","r") as new_score:
+            new_json = json.dump(scores, new_score)
 def start_flash():
     play_card(FLASHES)
 
@@ -40,27 +69,26 @@ def play_card(FLASH):
     word = data.iloc[random_word]
     swedish = word["word"]
     english= word["english"]
-    canvas.itemconfig(title,text="Swedish",)
-    canvas.itemconfig(meaning,text=swedish)
+    canvas.itemconfig(title,text="Swedish",fill = "black")
+    canvas.itemconfig(meaning,text=swedish,fill = "black")
     canvas.itemconfig(background_image,image = card_front_image)
     def show_answer():
         canvas.itemconfig(background_image,image = card_back_image)
-        canvas.itemconfig(title,text="English",)
-        canvas.itemconfig(meaning,text=english)
+        canvas.itemconfig(title,text="English", fill="white")
+        canvas.itemconfig(meaning,text=english, fill ="white")
     window.after(3000,show_answer)
 
     if FLASH>0:
          FLASH  -= 1
          window.after(6000, play_card,FLASH)
 
-
 # buttons
 right_image = PhotoImage(file=f"{file_dir}/images/right.png")
-right_button = Button(window, image=right_image)
+right_button = Button(window, image=right_image,command=right_score)
 right_button.grid(column=1,row=2)
 
 wrong_image = PhotoImage(height=100,width=100,file=f"{file_dir}/images/wrong.png")
-wrong_button = Button(window,image=wrong_image)
+wrong_button = Button(window,image=wrong_image, command=wrong_score)
 wrong_button.grid(column=0,row=2)
 
 
