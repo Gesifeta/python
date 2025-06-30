69736c0766f18297096ac45c3eec770846cc35c7
gesifeta <adamgemechu@gmail.com>
2025-06-30 20:38:40 +0300

    added check answer

--- day_34/quiz_brain.py
+++ day_34/quiz_brain.py
@@ -4,6 +4,21 @@ class Quiz:
         self.question_list = questions
         self.question_number = 0
         self.current_question = self.question_list[self.question_number]
-    
+
+
+    def check_answer(self, answer):
+        if self.current_question["answer"] == answer:
+            self.score +=1
+            return True
+        else:
+            return False
+        
+        
+    def get_next_question(self):
+         self.question_number += 1
+
+
 
     
+
+
