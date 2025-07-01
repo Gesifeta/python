c425c489f384c60663b95ca75cc141d129dd5a07
gesifeta <adamgemechu@gmail.com>
2025-07-01 15:51:10 +0300

    added padding to the canvas

--- day_34/ui.py
+++ day_34/ui.py
@@ -11,15 +11,16 @@ class QuizUI:
         self.wrong_image = PhotoImage(file=f"{file_path}/images/false.png")
         self.window.config(padx=20,pady=20,background=THEME_COLOR)
         self.window.title("Trivia Quizes")
-        self.canvas.grid(column=0,row=1,columnspan=2)
+        self.canvas.grid(column=0,row=1,columnspan=2,pady=(10,15))
         self.score_label = Label(text=f"Score: /10",pady=20,background=THEME_COLOR,fg="white")
         self.score_label.grid(column=1,row=0)
         self.right = Button(image=self.right_image,pady=10,background=THEME_COLOR,highlightthickness=0)
         self.right.grid(row=2,column=0)
         self.wrong = Button(image=self.wrong_image,pady=10,background=THEME_COLOR,highlightthickness=0)
         self.wrong.grid(row=2,column=1)
+
     def show_question(self, question):
-        self.canvas.create_text(150,150, text=question,width=250,fill="black",font=("Arial",15,"italic"))
+        self.question= self.canvas.create_text(150,150, text=question,width=250,fill="black",font=("Arial",15,"italic"))
 
         
         self.window.mainloop()
